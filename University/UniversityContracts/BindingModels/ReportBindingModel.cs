193a465a815e87c07cb7728e934ec19ecb6ca836
GokaPek <Hgogogo@yandex.ru>
2024-05-01 15:22:03 +0400

    начало репортов

---
+++ University/UniversityContracts/BindingModels/ReportBindingModel.cs
@@ -0,0 +1,15 @@
+﻿namespace UniversityContracts.BindingModels;
+
+/// <summary>
+/// Опции для сохранения отчета, при сохранении указать одно из двух
+/// </summary>
+public class ReportBindingModel
+{
+    public string? FileName { get; set; }
+    public Stream? Stream { get; set; }
+
+    /// <summary>
+    /// Массив айдишников по которым происходит выборка
+    /// </summary>
+    public int[]? Ids { get; set; }
+}
---
+++ University/UniversityContracts/BusinessLogicsContracts/IReportLogic.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UniversityContracts.BindingModels;
+
+namespace UniversityContracts.BusinessLogicContracts
+{
+    public interface IReportLogic
+    {
+        void SaveDisciplinesToWord(ReportBindingModel option);
+
+        void SaveDisciplinesToExcel(ReportBindingModel option);
+
+        void SendAccountsToEmail(ReportDateRangeBindingModel option, string email);
+        void SaveClientsToWord(ReportBindingModel option);
+
+        void SaveClientsToExcel(ReportBindingModel option);
+
+        void SendDisciplinesToEmail(ReportDateRangeBindingModel option, string email);
+    }
+}
