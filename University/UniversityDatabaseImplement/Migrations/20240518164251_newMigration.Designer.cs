2a5e8e33722f2d0c98876ee69eb4573c4c5cb6e7
DyCTaTOR <125912249+DyCTaTOR@users.noreply.github.com>
2024-05-18 20:48:42 +0400

    Попытка исправить другую ошибку

--- University/UniversityDatabaseImplement/Migrations/20240518164251_newMigration.Designer.cs
+++ University/UniversityDatabaseImplement/Migrations/20240518164251_newMigration.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using UniversityDatabaseImplement;
 
@@ -11,9 +12,11 @@ using UniversityDatabaseImplement;
 namespace UniversityDatabaseImplement.Migrations
 {
     [DbContext(typeof(UniversityDatabase))]
-    partial class UniversityDatabaseModelSnapshot : ModelSnapshot
+    [Migration("20240518164251_newMigration")]
+    partial class newMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ University/UniversityDatabaseImplement/Migrations/20240518164251_newMigration.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UniversityDatabaseImplement.Migrations
+{
+    /// <inheritdoc />
+    public partial class newMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateOnly>(
+                name: "Date",
+                table: "PlanOfStudys",
+                type: "date",
+                nullable: false,
+                defaultValue: new DateOnly(1, 1, 1));
+
+            migrationBuilder.AddColumn<DateOnly>(
+                name: "Date",
+                table: "Disciplines",
+                type: "date",
+                nullable: false,
+                defaultValue: new DateOnly(1, 1, 1));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Date",
+                table: "PlanOfStudys");
+
+            migrationBuilder.DropColumn(
+                name: "Date",
+                table: "Disciplines");
+        }
+    }
+}
